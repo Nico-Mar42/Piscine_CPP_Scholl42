b5b740a3ac62886e1e8a82cd999c83691980cd38
Nicolas Martin <nicolmar@c1r1p11.42lehavre.fr>
2025-06-03 13:51:53 +0200

    cpp 03 ex00 en cours

---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,32 @@
+#ifndef FIXED_HPP
+	#define FIXED_HPP
+
+# include <iostream>
+# include <string>
+# include <cstdlib>
+# include <fstream>
+# include <sstream>
+
+class ClapTrap
+{
+	//public:
+	//protected:
+	//private:
+
+	public:
+		ClapTrap();
+		ClapTrap(const ClapTrap& other);
+		ClapTrap& operator=(const ClapTrap& other);
+		~ClapTrap();
+
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+	private:
+		std::string _name;
+		unsigned int _hitPoints;
+		unsigned int _energyPoints;
+		unsigned int _attackDamage;
+};
+
+#endif
