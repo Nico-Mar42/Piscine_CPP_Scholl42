95b941fb4adf398cf7559c8dab7ad13b7ba2aeb4
Nicolas Martin <nicolmar@c1r1p11.42lehavre.fr>
2025-08-04 13:01:39 +0200

    cpp07 ex00

---
+++ cpp07/ex00/whatever.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
+
+template<typename T>
+void	swap(T &a, T &b)
+{
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template<typename T>
+T max(T const &a, T const &b)
+{
+	return(a >= b ? a : b);
+}
+
+template<typename T>
+T	min(T const &a, T const &b)
+{
+	return(a <= b ? a : b);
+}
